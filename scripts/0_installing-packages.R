3e93539ce73b9be9e56b36aef009988060d01454
Tolga Tezcan <77885260+ttezcann@users.noreply.github.com>
2026-04-02 16:11:08 -0700

    Add files via upload

---
+++ scripts/0_installing-packages.R
@@ -0,0 +1,13 @@
+### Installing all the packages and loading GSS data ###
+
+# This process may take 15-20 minutes.
+
+# Wait until the "Stop" sign in the console disappears and 
+#    no more code is running in the console.
+
+# After all the packages are installed (no "Stop" sign, no codes running in the console),
+# You should see "gss" and "key" under the "Environment - Data" part (top right)
+
+# If you see "gss" and "key" under the "Environment - Data" part (top right),
+# everything is all set.
+
