1f11c9aa7fcd1ad8c119091a0f8455f74464f20a
Tolga Tezcan <tolgatezcan@gmail.com>
2026-04-05 15:34:25 -0700

    s

--- scripts/0_load_gss_data.R
+++ scripts/0_load_gss_data.R
@@ -2,4 +2,6 @@
 temp <- tempfile(fileext = ".dta")
 download.file("https://raw.githubusercontent.com/ttezcann/regression-social-sciences/main/data/gss.dta", temp, mode = "wb")
 gss <- haven::read_dta(temp)
-key <- as.data.frame(sjlabelled::get_label(gss))
+invisible(capture.output(suppressMessages(suppressWarnings(
+  source(url("https://raw.githubusercontent.com/ttezcann/regression-social-sciences/main/scripts/0_relabel.R"))
+))))
